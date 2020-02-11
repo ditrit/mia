e8a3c430227aa733db7fa229e94e585cc40d1259
Fauconfan <joseph0priou@gmail.com>
2020-02-11 11:45:23 +0100

    fix db connection

--- core/database/databaseIAM.go
+++ core/database/databaseIAM.go
@@ -5,7 +5,7 @@ package database
 import (
 	"fmt"
 
-	model "iam/core/model"
+	"iam/core/model"
 
 	"github.com/jinzhu/gorm"
 	// necessary to gorm
@@ -30,6 +30,27 @@ func NewIAMDatabase(p string) IAMDatabase {
 	return res
 }
 
+func (idb *IAMDatabase) openConnection() {
+	db, err := gorm.Open("sqlite3", idb.pathToDB)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("failed to connect database")
+	}
+
+	idb.db = db
+}
+
+func (idb *IAMDatabase) closeConnection() {
+	err := idb.db.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("failed to close database")
+	}
+
+	idb.db = nil
+}
+
 //getListOfObjects :
 // returns the list of objects that will be represented in the database
 func (IAMDatabase) getListOfObjects() []interface{} {
@@ -48,28 +69,29 @@ func (IAMDatabase) getListOfObjects() []interface{} {
 
 func (idb IAMDatabase) dbInit() {
 	for _, elem := range idb.getListOfObjects() {
+		fmt.Printf("Create Table %T\n", elem)
 		idb.db.AutoMigrate(elem)
 	}
 
+	fmt.Printf("Create Empty Domain\n")
+
 	emptyModel, _ := model.NewDomain("")
+
 	idb.db.Create(&emptyModel)
 }
 
-func (idb IAMDatabase) dbReset() {
+func (idb IAMDatabase) dbDrop() {
 	for _, elem := range idb.getListOfObjects() {
+		fmt.Printf("Dropping Table %T\n", elem)
 		idb.db.DropTableIfExists(elem)
 	}
 }
 
 //Setup :
 func (idb IAMDatabase) Setup() {
-	db, err := gorm.Open("sqlite3", idb.pathToDB)
-	if err != nil {
-		fmt.Println(err.Error())
-		panic("failed to connect database")
-	}
-	defer db.Close() //nolint: errcheck
+	idb.openConnection()
+	defer idb.closeConnection() //nolint: errcheck
 
-	idb.dbReset()
+	idb.dbDrop()
 	idb.dbInit()
 }
